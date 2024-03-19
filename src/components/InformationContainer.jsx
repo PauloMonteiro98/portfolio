cbd451006ac7381a9061d8a0717e1f71ef469452
Joao Paulo da Silva Monteiro <jpcabal2@hotmail.com>
2024-03-18 22:01:59 -0300

    atualização informações e corrigindo problemas

---
+++ src/components/InformationContainer.jsx
@@ -0,0 +1,27 @@
+import { AiFillPhone, AiOutlineMail, AiFillEnvironment } from 'react-icons/ai';
+
+import '../styles/components/informationContainer.sass'
+
+const InformationContainer = () => {
+  return <section id="information">
+    <div className="info-card">
+        <AiFillPhone id='phone-icon' />
+        <div>
+            <h3>Telefone</h3>
+            <p>(84) 988357669</p>
+        </div>
+        <AiOutlineMail id='email-icon' />
+        <div>
+            <h3>E-mail</h3>
+            <p>paulomonteiro98@outlook.com</p>
+        </div>
+        <AiFillEnvironment id='pin-icon' />
+        <div>
+            <h3>Localização</h3>
+            <p>Natal / RN</p>
+        </div>
+    </div>
+  </section>
+}
+
+export default InformationContainer
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,16 @@
 import Avatar from '../img/16993052449711.jpeg'
+import SocialNetwork from './SocialNetwork'
 
 import '../styles/components/sidebar.sass'
+import InformationContainer from './InformationContainer'
 
 const Sidebar = () => {
   return (
     <aside id="sidebar">
         <img src={Avatar} alt="João Paulo Da Silva Monteiro" />
         <p className="title">Desenvolvedor Web</p>
-        <p>redes sociais</p>
+        <SocialNetwork />
+        <InformationContainer />
         <p>informações de contato</p>
         <a href="" className="btn">Download curriculo</a>
     </aside>
