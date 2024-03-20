06fa8933bcc55ff6aecc89a3a6ea5560e82de682
Joao Paulo da Silva Monteiro <jpcabal2@hotmail.com>
2024-03-20 19:52:19 -0300

    passos finais do portfolio

---
+++ src/components/ProjectsContainer.jsx
@@ -0,0 +1,13 @@
+
+
+const ProjectsContainer = () => {
+  return <section className="projects-container">
+    <h2>Projetos</h2>
+    <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime laboriosam, eius voluptas corporis, aliquam quisquam culpa ipsum sequi vero veniam rem, animi at adipisci nisi soluta. Illum tempore sint pariatur.
+    </p>
+    <a href="#" className="btn">Ver projetos</a>
+  </section>
+}
+
+export default ProjectsContainer
