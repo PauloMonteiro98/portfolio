b998fc2f371db17272b17823aedc82f0af3673e2
João Paulo <jpcabal2@hotmail.com>
2024-03-23 18:38:02 -0300

    atualizando visual

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -4,9 +4,9 @@ const ProjectsContainer = () => {
   return <section className="projects-container">
     <h2>Projetos</h2>
     <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime laboriosam, eius voluptas corporis, aliquam quisquam culpa ipsum sequi vero veniam rem, animi at adipisci nisi soluta. Illum tempore sint pariatur.
+        Você poderá ver meus projetos atuais disponibilizados no GitHub. Basta clicar no botão abaixo para mais informações!
     </p>
-    <a href="#" className="btn">Ver projetos</a>
+    <a href="https://github.com/PauloMonteiro98?tab=repositories" className="btn" target="_blank">Ver projetos</a>
   </section>
 }
 
